fcdde55b5cb412fc3ac91df8f9c45a1b1c61ab65
Taylor Bustamante <taylorbustamante16@gmail.com>
2020-12-09 10:02:18 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        "Hello, World!"
+    end 
+
+    get '/name' do
+        "My name is Taylor"
+    end
+
+    get '/hometown' do
+        "My hometown is Fresno"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Feathered Indians"
+    end
 end
